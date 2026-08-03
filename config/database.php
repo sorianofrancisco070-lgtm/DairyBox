0c42271dcc4db7dc23eb167ed2d1df81708c8f7b
Your Name <iscobar10112004@gmail.com>
2026-08-03 12:05:08 +0800

    Fix DB_NAME

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@
 define('DB_HOST', 'sakura.proxy.rlwy.net');
 define('DB_USER', 'root');
 define('DB_PASS', 'XrpDTsTYMBkAWcoFtkafnPRgPaadICZh');
-define('DB_NAME', 'railway');
+define('DB_NAME', 'Railway');
 define('DB_PORT', '52723');
 
 function getDB(): PDO {
